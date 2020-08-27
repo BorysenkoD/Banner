9c6fdffde07333e766d767163088aad508a13e9c
BorysenkoD <53504357+BorysenkoD@users.noreply.github.com>
2020-08-28 02:23:19 +0300

    минимальные исправления.

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,58 +1,71 @@
 <template>
-    <!--:class="{'choice-team':choiceTeamOne}"-->
-    <div class="center">
     <div class="banner">
-        <div class="container-left">
-            <div>
-                <img class="bet-logo" v-bind:src ="imageBet">
+        <div class="bet-logo">
+            <img class="img-bet-logo" :src="imageBet">
+        </div>
+
+        <div class="field-live">
+
+            <div class="live">
+                <span>Live</span>
             </div>
-            <div class="field-live">
-                <div class="center">
-                    <div class="live">Live</div>
-                </div>
-                <div class="type-bet">{{typeBet}}</div>
-                <q-btn class="button-team" @click="choiceOne(rateOne)"
-                       :class="{'choice-team':choiceTeamOne}">
-                    <img class="team-logo" v-bind:src="imageTeamOne">
-                    <div class="team-name"> {{teamNameOne}}</div>
-                    <div class="rate"> {{rateOne}}</div>
+
+            <div class="type-bet"> {{ typeBet }}</div>
+
+            <q-btn
+                    class="button-team"
+                    @click="choiceOne( rateOne )"
+                    :class="{ 'choice-team' : isChoiceTeam }">
+
+                <img class="team-logo" :src="imageTeamOne">
+                <div class="team-name"> {{ teamNameOne }}</div>
+                <div class="rate"> {{ rateOne }}</div>
+            </q-btn>
+
+            <q-btn class="button-team"
+                   @click="choiceTwo ( rateTwo )"
+                   :class="{'choice-team' : !isChoiceTeam}">
+
+                <img class="team-logo" :src="imageTeamTwo">
+                <div class="team-name"> {{ teamNameTwo }}</div>
+                <div class="rate"> {{ rateTwo }}</div>
+            </q-btn>
+
+        </div>
+        <div class="bet-field">
+            <div class="bet-size-choice">
+
+                <q-btn class="style-button"
+                       @click="rate1000"
+                       :class="{ 'choice-size' : size === 1000 }"> 1000 &#36;
                 </q-btn>
-                <q-btn class="button-team" @click="choiceTwo(rateTwo)"
-                       :class="{'choice-team':choiceTeamTwo}">
-                    <img class="team-logo" v-bind:src="imageTeamTwo">
-                    <div class="team-name"> {{teamNameTwo}}</div>
-                    <div class="rate"> {{rateTwo}}</div>
+
+                <q-btn class="style-button"
+                       @click="rate5000"
+                       :class="{ 'choice-size' : size === 5000  }"> 5000 &#36;
+                </q-btn>
+
+                <q-btn class="style-button"
+                       @click="rate10000"
+                       :class="{ 'choice-size' : size === 10000  }"> 10000 &#36;
                 </q-btn>
 
             </div>
-        </div>
-        <div class="container-right">
-            <div class = "bet-field">
-                <div class="bet-size-choice">
+            <div class="size-bet">
+                <span>Сумма ставки </span>
+                <q-input color=white bg-color=#ff6fff
+                         filled v-model="size">
+                    <template v-slot:append>
+                        <q-icon name="attach_money" color="white"/>
+                    </template>
+                </q-input>
 
-                    <q-btn class = "style-button"  @click="rate1000"
-                           :class="{'choice-size':choiceSizeOne}"> 1000 &#36;</q-btn>
-                    <q-btn class = "style-button"  @click="rate5000"
-                           :class="{'choice-size':choiceSizeFive}"> 5000 &#36;</q-btn>
-                    <q-btn class = "style-button"  @click="rate10000"
-                           :class="{'choice-size':choiceSizeTen}"> 10000 &#36;</q-btn>
-                </div>
-                <div class="size-bet">
-                    <q-input color=white bg-color=#ff6fff
-                             filled v-model="size"
-                             label="Сумма ставки:">
-                        <template v-slot:append>
-                            <q-icon name="attach_money" color="white"/>
-                        </template>
-                    </q-input>
-                </div>
-                <div class="bet-winning">
-                    <span> Выигрыш {{calculationAmountWin}}</span>
-                </div>
-                <q-btn class = "style-button"  href="www.egb.com" label="Сделать ставку"></q-btn>
             </div>
+            <div class="bet-winning">
+                <span> Выигрыш {{ calculationAmountWin }}</span>
+            </div>
+            <q-btn class="style-button" href="www.egb.com" label="Сделать ставку"></q-btn>
         </div>
-    </div>
     </div>
 </template>
 
@@ -62,187 +75,171 @@
             return {
                 teamNameOne: 'Cyberium',
                 teamNameTwo: 'MS Chonburi',
-                imageTeamOne:'../src/image/69.png',
-                imageTeamTwo:'../src/image/170.png',
-                imageBet:'../src/image/egb.png',
-                sizeWin: 0,
+                imageTeamOne: '../src/image/69.png',
+                imageTeamTwo: '../src/image/170.png',
+                imageBet: '../src/image/egb.png',
                 rateOne: 2.15,
                 rateTwo: 1.34,
                 bet: 0,
                 size: 0,
-                typeBet:'Победитель карты',
-                choiceTeamOne: null,
-                choiceTeamTwo: null,
-                choiceSizeOne: null,
-                choiceSizeFive: null,
-                choiceSizeTen: null,
+                typeBet: 'Победитель карты',
+                isChoiceTeam: null
             }
         },
         computed: {
             calculationAmountWin() {
-                return Math.round ( this.bet * this.size);
+                return Math.round(this.bet * this.size);
             }
         },
         methods: {
 
             choiceOne(cof) {
                 this.bet = cof;
-                this.choiceTeamOne = true;
-                this.choiceTeamTwo = false;
+                this.isChoiceTeam = true;
             },
+
             choiceTwo(cof) {
                 this.bet = cof;
-                this.choiceTeamOne = false;
-                this.choiceTeamTwo = true;
+                this.isChoiceTeam = false;
             },
 
             rate1000() {
-                this.size = 1000,
-                this.choiceSizeOne = true,
-                this.choiceSizeFive = false,
-                this.choiceSizeTen = false,
-                    console.log('1000')
+                this.size = 1000;
+                console.log('1000');
             },
+
             rate5000() {
-                this.size = 5000,
-                this.choiceSizeOne = false,
-                this.choiceSizeFive = true,
-                this.choiceSizeTen = false,
-                    console.log('5000' + this.choiceSizeOne + this.choiceSizeFive + this.choiceSizeTen  )
+                this.size = 5000;
+                console.log('5000');
             },
+
             rate10000() {
-                this.size = 10000,
-                    this.choiceSizeOne = false,
-                    this.choiceSizeFive = false,
-                    this.choiceSizeTen = true,
-                    console.log('10000')
+                this.size = 10000;
+                console.log('10000');
             }
         }
     }
 </script>
 
 <style scoped>
-    h1,h2,h3,h4,h5,h6 {
+    h1, h2, h3, h4, h5, h6 {
         margin: 0;
     }
-    .banner {
-        width: 649px;
-        height: auto;
-        background: linear-gradient(to top, #ff9900, #ffffff);
-        border: 1px rgba(204, 204, 204, 0.06);
 
+    .banner {
+        display: grid;
+        grid-template-areas: "logo betfield" "livefield betfield";
+        grid-template-rows: 1fr 2fr;
+        grid-template-columns: 60% 40%;
+        background: #6d6d6d;
     }
-    .container-left {
-        width: auto;
-        height: auto;
-        display: inline-block;
-        margin: 5px 5px 5px 5px;
-        text-align: center;
+
+    @media all and (max-width: 730px) {
+        .banner {
+            grid-template-areas: "logo" "livefield" "betfield";
+            grid-template-rows: 85px 1fr 1fr;
+            grid-template-columns: 100%;
+        }
     }
-    .container-right {
-        width: 300px;
-        height: auto;
-        display: inline-block;
-        margin: 5px 5px 5px 5px;
-        text-align: center;
-    }
+
     .bet-logo {
-       width: 300px;
-       height: auto;
-        margin: 10px;
+        grid-area: logo;
+        margin: auto;
     }
-    .team-logo{
+
+    .img-bet-logo {
+        max-width: 100%;
+        min-width: 350px;
+    }
+
+    .field-live {
+        grid-area: livefield;
+        display: grid;
+        width: 350px;
+        height: auto;
+        border: 2px solid #ff2b2b;
+    }
+
+    .team-name {
+        color: white;
+        font-size: 13pt;
+        margin: auto;
+    }
+
+    .team-logo {
         float: left;
         max-width: 65px;
         max-height: 40px;
         margin: auto;
     }
-    .team-name {
-        float: bottom;
-        color: white;
-        font-size: 13pt;
-        margin: auto;
-    }
+
     .rate {
         float: right;
         color: white;
         font-size: 20pt;
         margin: auto;
-
     }
-    .field-live {
-        width: 300px;
-        height: auto;
-        border: 2px solid #ff2b2b;
-        position: relative;
-        z-index: 1;
 
-    }
-    .live{
+    .live {
+        display: grid;
         width: 60px;
         height: 30px;
-        background: #ff6633;
+        background: #f57449;
         color: #ffffff;
-        position: absolute;
-        z-index: 50;
+        margin-left: auto;
+        margin-right: auto;
     }
-    .center{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: white;
-        font-size: 15pt;
-        position: relative;
-        z-index: 5;
-        margin-bottom: 10px;
-    }
+
     .type-bet {
         align-items: center;
         color: white;
+        margin-left: auto;
+        margin-right: auto;
 
     }
-    .bet-field {
-        display: inline-block;
-        list-style: none;
-        width: auto;
-        height: auto;
-    }
+
     .button-team {
-        display: flex;
+        display: grid;
         flex-wrap: wrap;
         width: 100%;
         height: 45px;
-        background:linear-gradient(to top, #f6a327, #f1d8cb);
+        background: #efba4c;
         border: 1px #ff6633;
-        margin: 5px auto;
-    }
-    .choice-team {
-        background: linear-gradient(to bottom, #904916, #f6864b);
     }
 
+    .choice-team {
+        background: #c96736;
+    }
+
+    .bet-field {
+        display: grid;
+        grid-area: betfield;
+        padding: 25px;
+    }
 
     .style-button {
-        background: linear-gradient(to top, #17a025, #afe095);
+        background: #47b153;
         color: white;
     }
+
     .choice-size {
-        background: linear-gradient(to bottom, #033909, #3b920e);
+        background: #238700;
     }
 
     .size-bet {
-        display: inline-block;
-
         margin: auto;
         color: white;
     }
+
     .bet-size-choice {
-        display: block;
         margin-left: auto;
         margin-right: auto;
         margin-bottom: 10px;
     }
+
     .bet-winning {
         color: #ffffff;
         font-size: 24pt;
     }
+
 </style>
